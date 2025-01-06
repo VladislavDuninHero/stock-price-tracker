b0844246077d4ca9abdcd2b6b771a451e05922c5
vladislavDunin <vladislavdunin.java@gmail.com>
2025-01-06 21:01:08 +0300

    init commit

---
+++ src/main/java/com/pet/stock_price_tracker/StockPriceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pet.stock_price_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPriceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPriceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pet/stock_price_tracker/StockPriceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.stock_price_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPriceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
